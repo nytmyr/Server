843918c292886c59c26446694aaefa44e8ae9145
hg <4683435+hgtw@users.noreply.github.com>
2022-11-05 11:13:39 -0400

    [Quest API] Add Lua handlers for zone controller events (#2514)
    This cleans up some of the NPC::Death event dispatch code.
    
    Adds handlers for EVENT_SPAWN_ZONE and EVENT_DEATH_ZONE used by zone
    controller and fixes the death handler exports which were incorrect.

--- zone/npc.h
+++ zone/npc.h
@@ -27,6 +27,7 @@
 #include "zonedump.h"
 #include "../common/loottable.h"
 
+#include <any>
 #include <deque>
 #include <list>
 
@@ -547,6 +548,9 @@ public:
 	void ReloadSpells();
 
 	static LootDropEntries_Struct NewLootDropEntry();
+
+	int DispatchZoneControllerEvent(QuestEventID evt, Mob* init, const std::string& data, uint32 extra, std::vector<std::any>* pointers);
+
 protected:
 
 	const NPCType*	NPCTypedata;
