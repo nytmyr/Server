ac232afe29025d5a3f8629386de54cfbbba7049d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-03-04 10:40:29 -0600

    [Bug Fix] Ensure synchronization of pet taunt state with UI (#3025)
    * fix to desync between pet taunt state and button
    
    * Update npc.h
    
    * Update npc.cpp
    
    * Update npc.h
    
    ---------
    
    Co-authored-by: Alex King <89047260+Kinglykrab@users.noreply.github.com>

--- zone/npc.cpp
+++ zone/npc.cpp
@@ -3974,3 +3974,11 @@ void NPC::HandleRoambox()
 
 	return;
 }
+
+void NPC::SetTaunting(bool is_taunting) {
+	taunting = is_taunting;
+
+	if (IsPet() && IsPetOwnerClient()) {
+		GetOwner()->CastToClient()->SetPetCommandState(PET_BUTTON_TAUNT, is_taunting);
+	}
+}
