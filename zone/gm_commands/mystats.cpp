3544e6449afe984b16b5bacc59244318682b0894
nytmyr <53322305+nytmyr@users.noreply.github.com>
2023-04-24 09:53:57 -0500

    [Features] #mystats will show bot stats if it is a bot you own
    This will enable #mystats to show stats on bots if the bot is owned by the player.

--- zone/gm_commands/mystats.cpp
+++ zone/gm_commands/mystats.cpp
@@ -2,13 +2,11 @@
 
 void command_mystats(Client *c, const Seperator *sep)
 {
-	if (c->GetTarget() && c->GetPet()) {
-		if (c->GetTarget()->IsPet() && c->GetTarget() == c->GetPet()) {
-			c->GetTarget()->ShowStats(c);
-		}
-		else {
-			c->ShowStats(c);
-		}
+	if (c->GetTarget() && c->GetPet() && (c->GetTarget()->IsPet() && c->GetTarget() == c->GetPet())) {
+		c->GetTarget()->ShowStats(c);
+	}
+	else if (c->GetTarget() && c->GetTarget()->IsBot() && (c->GetTarget()->GetOwner()->CastToClient()->CharacterID() == c->CharacterID())) {
+		c->GetTarget()->ShowStats(c);
 	}
 	else {
 		c->ShowStats(c);
