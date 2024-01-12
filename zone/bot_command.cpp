78e1b93d8378df003a1f3d68497beb9b87067285
nytmyr <53322305+nytmyr@users.noreply.github.com>
2024-01-12 12:39:11 -0600

    [Bots] Add actionable support to ^report

--- zone/bot_command.cpp
+++ zone/bot_command.cpp
@@ -18602,27 +18602,25 @@ void bot_subcommand_bot_report(Client *c, const Seperator *sep)
 	if (helper_command_alias_fail(c, "bot_subcommand_bot_report", sep->arg[0], "botreport"))
 		return;
 	if (helper_is_help_or_usage(sep->arg[1])) {
-		c->Message(Chat::White, "usage: %s ([actionable: target | byname | ownergroup | ownerraid | targetgroup | namesgroup | spawned] ([actionable_name]))", sep->arg[0]);
+		c->Message(Chat::White, "usage: %s ([actionable: target | byname | ownergroup | ownerraid | targetgroup | namesgroup | mmr | byclass | byrace | spawned] ([actionable_name]))", sep->arg[0]);
 		return;
 	}
-	const int ab_mask = ActionableBots::ABM_NoFilter;
+	const int ab_mask = ActionableBots::ABM_Type1;
 
-	std::string ab_type_arg = sep->arg[1];
-	if (ab_type_arg.empty()) {
-		if (c->GetTarget()) {
-			if (c->GetTarget()->IsClient() && c->GetTarget()->CastToClient() == c)
-				ab_type_arg = "ownergroup";
-			else if (c->GetTarget()->IsClient() && c->GetTarget()->CastToClient() != c)
-				ab_type_arg = "targetgroup";
-		}
-		else {
-			ab_type_arg = "spawned";
-		}
+	std::string class_race_arg = sep->arg[1];
+	bool class_race_check = false;
+	if (!class_race_arg.compare("byclass") || !class_race_arg.compare("byrace")) {
+		class_race_check = true;
 	}
 
 	std::list<Bot*> sbl;
-	if (ActionableBots::PopulateSBL(c, ab_type_arg.c_str(), sbl, ab_mask, sep->arg[2]) == ActionableBots::ABT_None)
+	if (ActionableBots::PopulateSBL(c, sep->arg[1], sbl, ab_mask, !class_race_check ? sep->arg[2] : nullptr, class_race_check ? atoi(sep->arg[2]) : 0) == ActionableBots::ABT_None) {
 		return;
+	}
+
+	sbl.remove(nullptr);
+
+	auto sbl_size = sbl.size();
 
 	for (auto bot_iter : sbl) {
 		if (!bot_iter)
